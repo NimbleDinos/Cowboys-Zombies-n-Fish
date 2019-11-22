329a41d8cf95fa2afbb2cc59c4ff4946fab45562
unknown <cwells1298@gmail.com>
2019-11-22 23:35:43 +0000

    May be work now pls

---
+++ Gitsucks the Jam/Cowbys n Fish/Assets/Scripts/TimeHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeHandler : MonoBehaviour
+{
+	public static float zombieSpawnerTime = 60.0f;
+	[Min(0.0f)]
+	public static float timeTillZombieSpawn;
+
+	public static float inputReceiveTime = 15.0f;
+	[Min(0.0f)]
+	public static float timeTillReceiveInput;
+
+	private void Start()
+	{
+		timeTillZombieSpawn = zombieSpawnerTime;
+		timeTillReceiveInput = inputReceiveTime;
+	}
+
+	private void Update()
+	{
+		timeTillZombieSpawn -= Time.deltaTime;
+		timeTillReceiveInput -= Time.deltaTime;
+	}
+}
