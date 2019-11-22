825412e7f23b8a222d888669cf515ec49b77888b
JJMaslen <44002981+JJMaslen@users.noreply.github.com>
2019-11-22 20:31:48 +0000

    Discord Bot set up with basic commands

---
+++ TheSheriff/TheSheriff/SheriffRun.bat
@@ -0,0 +1,1 @@
+start cmd.exe /k "C:\Users\computing\Downloads\Cowboys-Zombies-n-Fish\TheSheriff\TheSheriff\TheSheriff.py"
---
+++ TheSheriff/TheSheriff/TheSheriff.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+
+bot_prefix = "?"
+client = commands.Bot(command_prefix=bot_prefix)
+client.remove_command("help")
+
+@client.event
+async def on_ready():
+	print("The Sheriff is online")
+	print("Name: The Sheriff")
+	print("TD: {}".format(client.user.id))
+
+@client.event
+async def on_message(message):
+	if message.author.bot:
+		return
+
+	if "texttest" in message.content.lower():
+		await message.channel.send("on_message test")
+
+	await client.process_commands(message)
+
+@client.command()
+async def test(ctx):
+	await ctx.send("Test Command")
+
+@client.command()
+async def howdy(ctx):
+	await ctx.send("Howdy Partner!")
+
+client.run("NjQ3NTEzNTYyNzY3NTU2NjM4.Xdgymw.a9AqmltyAYbrAoDWeCZ4Vjnp6zM")
