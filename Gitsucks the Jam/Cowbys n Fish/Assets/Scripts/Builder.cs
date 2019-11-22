721302c787366301f365aa7d73999e0e22245bdc
cammy101 <45818561+cammy101@users.noreply.github.com>
2019-11-22 23:21:52 +0000

    try again

---
+++ Gitsucks the Jam/Cowbys n Fish/Assets/Scripts/Builder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+	//public int FishInPool;
+	public int Barricade;
+
+	public int Fish;
+
+	public FishBucket fishPool;
+	public GameObject barricadeObject;
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		Fish = fishPool.FishInPool;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		buyBarricade();
+		placeBarricade();
+	}
+
+	public void buyBarricade()
+	{
+		if (fishPool.FishInPool >= 10 && Input.GetKeyDown(KeyCode.P))
+		{
+			Debug.LogError("Buying");
+			Barricade++;
+			fishPool.FishInPool -= 10;
+		}
+	}
+
+	public void placeBarricade()
+	{
+		if (Barricade >= 1 && Input.GetKeyDown(KeyCode.L))
+		{
+			Barricade--;
+			Instantiate(barricadeObject, transform.position, Quaternion.identity);
+		}
+	}
+}
