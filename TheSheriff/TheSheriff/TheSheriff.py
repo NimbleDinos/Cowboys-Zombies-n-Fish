a6c855a3073d3489fa04849292b4ff346c45f700
Lottie Bryant <amaliciabryant@gmail.com>
2019-11-24 10:11:44 +0000

    fixed the timer

--- TheSheriff/TheSheriff/TheSheriff.py
+++ TheSheriff/TheSheriff/TheSheriff.py
@@ -7,20 +7,28 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 
 import UserInfo
-#import requests
+import requests
+import threading
 
 bot_prefix = "?"
 client = commands.Bot(command_prefix=bot_prefix)
 client.remove_command("help")
 
-playerList = []
+playerList = {}
 
-unitList = ['fisher', 'attacker', 'defender', 'builder']
 placeList = ['near', 'mid', 'far']
 acceptedClasses = ['fisher', 'attacker', 'builder']
 
 baseUrl = "http://localhost:8081/"
 
+
+def checkTime():
+	threading.Timer(15.0, checkTime).start()
+	for k in playerList.keys():
+		playerList[k] = 0
+
+
+checkTime()
 
 @client.event
 async def on_ready():
@@ -35,14 +43,14 @@ async def joingame(ctx):
 	userName = ctx.message.author.name
 	returned = await UserInfo.newPlayer(userID, userName, playerList)
 
-	if returned == 0:
+	if returned == 1:
 		await ctx.send(userName + " you are already in the game!")
-	elif returned == 1:
+	elif returned == 0:
 		try:
-			playerList.append([userID, 0])
+			playerList[userID] = 1
 			joinGameUrl = baseUrl + "command?action=join&userId={0}".format(userID)
 			response = requests.get(joinGameUrl)
-			if (response.status_code == 200):
+			if response.status_code == 200:
 				await ctx.send(userName + " you have entered the game!")
 		except:
 			await ctx.send("Whoa there {0}! Somethin' ain't right here".format(userName))
@@ -53,14 +61,17 @@ async def chooseClass(ctx, clas):
 	userName = ctx.message.author.name
 
 	if userID not in playerList:
-		ctx.send("Whoa there {0}, you need to join the game first!".format(userName))
+		await ctx.send("Whoa there {0}, you need to join the game first!".format(userName))
+	elif playerList[userID] == 1:
+		await ctx.send("Let's not rush {0}! You already did ya thing this turn!".format(userName))
 	else:
 		if clas in acceptedClasses:
 			try:
 				chooseClassGameurl = baseUrl + "command?action=chooseClass&userId={0}&class={1}".format(userID, clas)
 				response = requests.get(chooseClassGameurl)
-				if (response.status_code == 200):
+				if response.status_code == 200:
 					await ctx.send("Good luck out there, {0}!".format(userName))
+					playerList[userID] = 1
 			except:
 				await ctx.send("Whoa there {0}! Somethin' ain't right here".format(userName))
 		else:
@@ -72,7 +83,9 @@ async def move(ctx, x, y):
 	userName = ctx.message.author.name
 
 	if userID not in playerList:
-		ctx.send("Whoa there {0}, you need to join the game first!".format(userName))
+		await ctx.send("Whoa there {0}, you need to join the game first!".format(userName))
+	elif playerList[userID] == 1:
+		await ctx.send("Let's not rush {0}! You already did ya thing this turn!".format(userName))
 	else:
 		MAX_X = 100
 		MAX_Y = 50
@@ -82,8 +95,9 @@ async def move(ctx, x, y):
 			try:
 				moveUrl = baseUrl + "command?action=move&userId={0}&x={1}&y={2}".format(userID, x, y)
 				response = requests.get(moveUrl)
-				if (response.status_code == 200):
+				if response.status_code == 200:
 					await ctx.send("See ya soon cowboy")
+					playerList[userID] = 1
 			except:
 				await ctx.send("Whoa there {0}! Somethin' ain't right here".format(userName))
 		else:
@@ -95,17 +109,19 @@ async def action(ctx):
 	userName = ctx.message.author.name
 	
 	if userID not in playerList:
-		ctx.send("Whoa there {0}, you need to join the game first!".format(userName))
+		await ctx.send("Whoa there {0}, you need to join the game first!".format(userName))
+	elif playerList[userID] == 1:
+		await ctx.send("Let's not rush {0}! You already did ya thing this turn!".format(userName))
 	else:
 		try:
 			for item in playerList:
 				if item == userID:
-					playerDoList[item] = 1
 					print(item)
 			joinGameUrl = baseUrl + "command?action=action&userId={0}".format(userID)
 			response = requests.get(joinGameUrl)
-			if (response.status_code == 200):
+			if response.status_code == 200:
 				await ctx.send(userName + " go do your thing partner!")
+				playerList[userID] = 1
 		except:
 			await ctx.send("Whoa there {0}! Somethin' ain't right here".format(userName))
 
@@ -121,6 +137,7 @@ async def on_message(message):
 	if "texttest" in message.content.lower():
 		await message.channel.send("on_message test")
 		await UserInfo.infoUpdate()
+
 
 	await client.process_commands(message)
 
